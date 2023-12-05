0b9b800f2e1e279d975c135acfae6e6e6d5a7675
jaypatel_simform <jaypatel@simformsolutions.com>
2023-12-05 13:52:23 +0530

    feat: created logger service and injected in calculator service and use it in test cases using spyOn.

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -1,21 +1,32 @@
+import { LoggerService } from '../logger/logger.service';
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
   it('Should add two numbers', () => {
     // pending() //// for pending test case
     // xit() instead of it() for pending state
-    const calculator = new CalculatorService();
+    let loggerService = new LoggerService();
+    // .and.callTrough() call original services along with spyOn
+    spyOn(loggerService, 'log').and.callThrough();
+    const calculator = new CalculatorService(loggerService);
     let result = calculator.add(2, 2);
     expect(result).toBe(4);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
   it('Should subtract two numbers', () => {
-    const calculator = new CalculatorService();
+    let loggerService = new LoggerService();
+    spyOn(loggerService, 'log').and.callThrough();
+    const calculator = new CalculatorService(loggerService);
     let result = calculator.subtract(6, 3);
     expect(result).toBe(3);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
   it('Should subtract two negative numbers', () => {
-    const calculator = new CalculatorService();
+    let loggerService = new LoggerService();
+    spyOn(loggerService, 'log').and.callThrough();
+    const calculator = new CalculatorService(loggerService);
     let result = calculator.subtract(-6, -3);
     expect(result).toBe(-3);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 });
--- src/app/services/calculator/calculator.service.ts
+++ src/app/services/calculator/calculator.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+import { LoggerService } from '../logger/logger.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CalculatorService {
+  constructor(private loggerService: LoggerService) {}
   add(n1: number, n2: number) {
+    this.loggerService.log('Add Operation is called');
     return n1 + n2;
   }
   subtract(n1: number, n2: number) {
+    this.loggerService.log('Subtract Operation is called');
     return n1 - n2;
   }
 }
