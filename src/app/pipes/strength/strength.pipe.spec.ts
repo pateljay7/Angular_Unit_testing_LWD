e29a57a84961fd9ee9695490d9855d10807daaff
jaypatel_simform <jaypatel@simformsolutions.com>
2023-12-05 15:02:26 +0530

    feat: test case for angular pipe using isolated unit test

---
+++ src/app/pipes/strength/strength.pipe.spec.ts
@@ -0,0 +1,24 @@
+import { StrengthPipe } from './strength.pipe';
+
+describe('StrengthPipe', () => {
+  it('create an instance', () => {
+    const pipe = new StrengthPipe();
+    expect(pipe).toBeTruthy();
+  });
+  it('should display weak if value is less then 10', () => {
+    const pipe = new StrengthPipe();
+    let number = Math.floor(Math.random() * 10);
+    expect(pipe.transform(number)).toEqual(`${number} (weak)`);
+  });
+  it('should display strong if value is >=10 and <20', () => {
+    const pipe = new StrengthPipe();
+    let number = Math.floor(Math.random() * (20 - 10 + 1)) + 10;
+    expect(pipe.transform(number)).toEqual(`${number} (strong)`);
+  });
+  it('should display strongest if value is >20', () => {
+    const pipe = new StrengthPipe();
+    let number =
+      Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 20)) + 20;
+    expect(pipe.transform(number)).toEqual(`${number} (strongest)`);
+  });
+});
