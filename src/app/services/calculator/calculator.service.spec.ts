701686dd91c73b213249191ce960ec5cac56114f
jaypatel_simform <jaypatel@simformsolutions.com>
2023-12-05 14:18:18 +0530

    feat: use of beforeEach in calculator test case

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -2,29 +2,28 @@ import { LoggerService } from '../logger/logger.service';
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
+  let mockLoggerService: any, calculator: any;
+  beforeEach(() => {
+    // let loggerService = new LoggerService();
+    // mock logger service for spy,it will not touch original service
+    mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(mockLoggerService);
+  });
   it('Should add two numbers', () => {
     // pending() //// for pending test case
     // xit() instead of it() for pending state
-    // let loggerService = new LoggerService();
-    // mock logger service for spy,it will not touch original service
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
     // .and.callTrough() call original services along with spyOn
     // spyOn(loggerService, 'log').and.callThrough();
-    const calculator = new CalculatorService(mockLoggerService);
     let result = calculator.add(2, 2);
     expect(result).toBe(4);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
   it('Should subtract two numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(mockLoggerService);
     let result = calculator.subtract(6, 3);
     expect(result).toBe(3);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
   it('Should subtract two negative numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(mockLoggerService);
     let result = calculator.subtract(-6, -3);
     expect(result).toBe(-3);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
