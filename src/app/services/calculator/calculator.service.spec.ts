633d809c44ea6ee4b930f561f5518904fe3d928e
jaypatel_simform <jaypatel@simformsolutions.com>
2023-12-05 13:59:55 +0530

    feat: created mock logger service using creatSpyObj in testcase

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -5,28 +5,28 @@ describe('CalculatorService', () => {
   it('Should add two numbers', () => {
     // pending() //// for pending test case
     // xit() instead of it() for pending state
-    let loggerService = new LoggerService();
+    // let loggerService = new LoggerService();
+    // mock logger service for spy,it will not touch original service
+    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
     // .and.callTrough() call original services along with spyOn
-    spyOn(loggerService, 'log').and.callThrough();
-    const calculator = new CalculatorService(loggerService);
+    // spyOn(loggerService, 'log').and.callThrough();
+    const calculator = new CalculatorService(mockLoggerService);
     let result = calculator.add(2, 2);
     expect(result).toBe(4);
-    expect(loggerService.log).toHaveBeenCalledTimes(1);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
   it('Should subtract two numbers', () => {
-    let loggerService = new LoggerService();
-    spyOn(loggerService, 'log').and.callThrough();
-    const calculator = new CalculatorService(loggerService);
+    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    const calculator = new CalculatorService(mockLoggerService);
     let result = calculator.subtract(6, 3);
     expect(result).toBe(3);
-    expect(loggerService.log).toHaveBeenCalledTimes(1);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
   it('Should subtract two negative numbers', () => {
-    let loggerService = new LoggerService();
-    spyOn(loggerService, 'log').and.callThrough();
-    const calculator = new CalculatorService(loggerService);
+    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    const calculator = new CalculatorService(mockLoggerService);
     let result = calculator.subtract(-6, -3);
     expect(result).toBe(-3);
-    expect(loggerService.log).toHaveBeenCalledTimes(1);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 });
