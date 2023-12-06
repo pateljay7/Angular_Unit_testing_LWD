0f1dd3c7d0a9cb416a2d1ef3c53349a1f16336d9
jaypatel_simform <jaypatel@simformsolutions.com>
2023-12-06 15:13:11 +0530

    feat: create single post component instance using angular TestBed componentFixture and parse component template using angular TestBed

--- src/app/components/posts/single-post/single-post.component.spec.ts
+++ src/app/components/posts/single-post/single-post.component.spec.ts
@@ -1,17 +1,38 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { first } from 'rxjs';
 import { Post } from 'src/app/models/post';
 import { SinglePostComponent } from './single-post.component';
 
 describe('Post Component', () => {
-  it('should raise and event when the delete post is clicked', () => {
-    const component = new SinglePostComponent();
+  let component: SinglePostComponent;
+  let fixture: ComponentFixture<SinglePostComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SinglePostComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
+    fixture = TestBed.createComponent(SinglePostComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create post component using TestBed', () => {
+    expect(component).toBeDefined();
+  });
+  it('should render the post title in the anchor element', () => {
     const post: Post = { id: 1, body: 'body 1', title: 'title 1' };
     component.post = post;
-
+    fixture.detectChanges(); // to detect changes in html page
+    const postElement: HTMLElement = fixture.nativeElement;
+    const a = postElement.querySelector('a');
+    expect(a?.textContent).toContain(post.title);
+  });
+  it('should raise and event when the delete post is clicked', () => {
+    const post: Post = { id: 1, body: 'body 1', title: 'title 1' };
+    component.post = post;
     component.delete.pipe(first()).subscribe((selectedPost) => {
       expect(selectedPost).toBe(post);
     });
-
     component.onDeletePost(new MouseEvent('click'));
   });
 });
